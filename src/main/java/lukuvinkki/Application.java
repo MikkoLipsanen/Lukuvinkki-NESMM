03637ba98c23303fc81469c43715affa327381c5
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2018-11-22 21:19:10 +0200

    deleted uncommented lines from main

--- src/main/java/lukuvinkki/Application.java
+++ src/main/java/lukuvinkki/Application.java
@@ -16,36 +16,5 @@ public class Application {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         SpringApplication app = new SpringApplication(Application.class);
         app.run(args);
-        
-//            Class.forName ("org.h2.Driver");
-
-    // Enable logging
-    // DriverManager.setLogStream(System.err);
-
-//    String URL = "jdbc:h2:mem:example-app";
-    
-//    System.out.println("Getting Connection");
-//    Connection c = DriverManager.getConnection (
-//      "jdbc:h2:mem:example-app",
-//      "ha", "");  // user, passwd
-//    DatabaseMetaData md = c.getMetaData();
-//    ResultSet rs = md.getTables(null, null, "%", null);
-//    while (rs.next()) {
-//      System.out.println(rs.getString(3));
-//    }
-//    }
-    
-//    Connection conn = DriverManager.getConnection(URL, "user", "passw");
-//    
-//        DatabaseMetaData dmd = conn.getMetaData();
-//        ResultSet rs1 = dmd.getSchemas();
-//        while (rs1.next()) {
-//          String ss = rs1.getString(1);
-//          ResultSet rs2 = dmd.getTables(null, ss, "%", null);
-//          while (rs2.next()) {
-//                System.out.println(rs2.getString(3) + " " + rs2.getString(4));
-//            }
-//        }
-//        conn.close();
     }
 }
